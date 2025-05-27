9ada43378ed3477ce738897dc0682f9a5d7f4c3f
James Connolly <me@j-conn.com>
2025-05-27 11:35:19 -0400

    print device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use pollster::FutureExt;
+
 fn main() {
-	println!("Hello, world!");
+	test().block_on();
+}
+
+async fn test() {
+	let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+		backends: wgpu::Backends::all(),
+		flags: wgpu::InstanceFlags::VALIDATION,
+		backend_options: wgpu::BackendOptions::default()
+	});
+
+	let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions::default()).await.unwrap();
+
+	let (device, _queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+		required_features: wgpu::Features::empty(),
+		required_limits: wgpu::Limits::default(),
+		memory_hints: wgpu::MemoryHints::Performance,
+		label: None,
+		trace: wgpu::Trace::Off
+	}).await.unwrap();
+
+	println!("{device:?}");
 }
